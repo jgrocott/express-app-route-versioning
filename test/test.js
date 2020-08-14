fff3fefa578f8ad981afc409620a7472dce71588
Justin Grocott <justin@jmgmedia.com.au>
2020-08-14 23:17:56 +1000

    Fixed unit tests

--- test/test.js
+++ test/test.js
@@ -1,168 +1,159 @@
-var routesVersioning = require('./../index')();
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
+const routesVersioning = require('./../index')();
 
 describe('routes versioning', function() {
-   var req;
-   var res;
-   var next;
-   beforeEach(function() {
-      req = {};
-      res = {};
-      next = function() {};
-   });
-   it('calling routesVersioning should ' +
-      'return connect style middleware',
-      function() {
-         var middleware = routesVersioning({});
-         assert.equal(typeof(middleware), 'function');
-      });
-   it('calling routesVersioning with invalid input, ' +
-      'should thrown an error',
-      function() {
-         assert.equal(routesVersioning([]), -1);
-         assert.equal(routesVersioning(null), -1);
-         assert.equal(routesVersioning(''), -1);
-      });
+  let req;
+  let res;
+  let next;
+  let app;
+  let use;
+  beforeEach(function() {
+    req = {};
+    res = {};
+    next = function() {};
 
-   it('if version is not provided by client, ' +
-      'and NoMatchFoundCallback if provided is called',
-      function() {
-         var NoMatchFoundSpy = sinon.spy();
-         var middleware = routesVersioning({}, NoMatchFoundSpy);
-         middleware(req, res, next);
-         assert.ok(NoMatchFoundSpy.calledOnce);
-         assert.ok(NoMatchFoundSpy.calledWith(req, res, next));
-      });
+    use = function(cb) {
+      cb(req, res, next);
+    };
 
-   it('if version if not provided by client, ' +
-      'and NoMatchFoundCallback is not provided, ' +
-      'latest version callback should be called',
-      function() {
-         var latestVersionSpy = sinon.spy();
-         var middleware = routesVersioning({
-            "1.2.1": sinon.spy(),
-            "1.3.1": latestVersionSpy
-         });
-         middleware(req, res, next);
-         assert.ok(latestVersionSpy.calledOnce);
-         assert.ok(latestVersionSpy.calledWith(req, res, next));
-      });
+    app = { request: {}, use };
+  });
+  it('calling routesVersioning should return connect style middleware', function() {
+    const middleware = routesVersioning({});
+    assert.equal(typeof middleware, 'function');
+  });
+  it('calling routesVersioning with invalid input, should thrown an error', function() {
+    assert.equal(routesVersioning([]), -1);
+    assert.equal(routesVersioning(null), -1);
+    assert.equal(routesVersioning(''), -1);
+  });
 
-      it('if accept-version header is present, ' +
-         'appropriate callback should be called',
-         function() {
-             var version1Spy = sinon.spy();
-             var version2Spy = sinon.spy();
-            var middleware = routesVersioning({
-               "1.2.1": version1Spy,
-               "1.3.1": version2Spy
-            });
-            req.headers = {};
-            req.headers["accept-version"] = "1.2.1";
-            middleware(req, res, next);
-            assert.ok(version1Spy.calledOnce);
-            assert.ok(version1Spy.calledWith(req, res, next));
-         });
-   it('when multiple version are provided, ' +
-      'matching version should be called',
-      function() {
-         var version1Spy = sinon.spy();
-         var version2Spy = sinon.spy();
-         var middleware = routesVersioning({
-            "1.2.1": version1Spy,
-            "2.3.1": version2Spy
-         });
-         req.version = "2.3.1";
-         middleware(req, res, next);
-         assert.ok(version2Spy.calledOnce);
-         assert.ok(version2Spy.calledWith(req, res, next));
-         assert.ok(version1Spy.neverCalledWith(req, res, next));
+  it('if version is not provided by client, call NoMatchFoundCallback if provided', function() {
+    const NoMatchFoundSpy = sinon.spy();
+    const middleware = routesVersioning({}, NoMatchFoundSpy);
+    middleware(app);
+    assert.ok(NoMatchFoundSpy.calledOnce);
+    assert.ok(NoMatchFoundSpy.calledWith(req, res, next));
+  });
+
+  it('if version if not provided by client, and NoMatchFoundCallback is not provided, latest version callback should be called', function() {
+    const latestVersionSpy = sinon.spy();
+    routesVersioning({
+      '1.2.1': sinon.spy(),
+      '1.3.1': latestVersionSpy,
+    })(app);
+    assert.ok(latestVersionSpy.calledOnce);
+    assert.ok(latestVersionSpy.calledWith(app));
+  });
+
+  it('if accept-version header is present, appropriate callback should be called', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '1.2.1': version1Spy,
+      '1.3.1': version2Spy,
+    });
+    app.request.headers = {};
+    app.request.headers['accept-version'] = '1.2.1';
+    middleware(app);
+    assert.ok(version1Spy.calledOnce);
+    assert.ok(version1Spy.calledWith(app));
+  });
+  it('when multiple version are provided, matching version should be called', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '1.2.1': version1Spy,
+      '2.3.1': version2Spy,
+    });
+    app.request.version = '2.3.1';
+    middleware(app);
+    assert.ok(version2Spy.calledOnce);
+    assert.ok(version2Spy.calledWith(app));
+    assert.ok(version1Spy.neverCalledWith(app));
+  });
+  it('when multiple version are provided, and no matching version found NoMatchFoundCallback is called', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const NoMatchFoundSpy = sinon.spy();
+    const middleware = routesVersioning(
+      {
+        '1.2.1': version1Spy,
+        '2.3.1': version2Spy,
+      },
+      NoMatchFoundSpy
+    );
+    app.request.version = '2.3.2';
+    middleware(app);
+    assert.ok(NoMatchFoundSpy.calledOnce);
+    assert.ok(NoMatchFoundSpy.calledWith(req, res, next));
+  });
+  it('when multiple version are provided, and no matching version found latest version is called, but only if NoMatchFoundCallback is not provided', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '2.0.2': sinon.spy(),
+      '~2.3.0': sinon.spy(),
+      '1.2.1': version1Spy,
+      '2.0.0': sinon.spy(),
+      '~1.4.4': sinon.spy(),
+      '1.22.0': sinon.spy(),
+      '2.3.1': version2Spy,
+    });
+    app.request.version = '1.3.2';
+    middleware(app);
+    assert.ok(version2Spy.calledOnce);
+    assert.ok(version2Spy.calledWith(app));
+  });
+  it('when ^ is used in version, version should matching appropriately', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '^1.2.1': version1Spy,
+      '2.3.1': version2Spy,
+    });
+    app.request.version = '1.4.2';
+    middleware(app);
+    assert.ok(version1Spy.calledOnce);
+    assert.ok(version1Spy.calledWith(app));
+  });
+  it('when ~ is used in version, version should matching appropriately', function() {
+    const version1Spy = sinon.spy();
+    const version2Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '~1.2.1': version1Spy,
+      '2.3.1': version2Spy,
+    });
+    app.request.version = '1.2.9';
+    middleware(app);
+    assert.ok(version1Spy.calledOnce);
+    assert.ok(version1Spy.calledWith(app));
+  });
+  it(
+    'when ~ is used in version, version should matching appropriately, ' +
+      'if doesnt match, highest version should be called',
+    function() {
+      const version1Spy = sinon.spy();
+      const version2Spy = sinon.spy();
+      const middleware = routesVersioning({
+        '~1.2.1': version1Spy,
+        '2.3.1': version2Spy,
       });
-   it('when multiple version are provided, and no matching version found ' +
-      'NoMatchFoundCallback is called',
-      function() {
-         var version1Spy = sinon.spy();
-         var version2Spy = sinon.spy();
-         var NoMatchFoundSpy = sinon.spy();
-         var middleware = routesVersioning({
-            "1.2.1": version1Spy,
-            "2.3.1": version2Spy
-         }, NoMatchFoundSpy);
-         req.version = "2.3.2";
-         middleware(req, res, next);
-         assert.ok(NoMatchFoundSpy.calledOnce);
-         assert.ok(NoMatchFoundSpy.calledWith(req, res, next));
-      });
-   it('when multiple version are provided, and no matching version found ' +
-      'latest version is called, if NoMatchFoundCallback is not provided',
-      function() {
-         var version1Spy = sinon.spy();
-         var version2Spy = sinon.spy();
-         var middleware = routesVersioning({
-             "2.0.2": sinon.spy(),
-             "~2.3.0": sinon.spy(),
-            "1.2.1": version1Spy,
-            "2.0.0": sinon.spy(),
-            "~1.4.4": sinon.spy(),
-            "1.22.0": sinon.spy(),
-            "2.3.1": version2Spy,
-            "null": sinon.spy()
-         });
-         req.version = "1.3.2";
-         middleware(req, res, next);
-         assert.ok(version2Spy.calledOnce);
-         assert.ok(version2Spy.calledWith(req, res, next));
-      });
-   it('when ^ is used in version, version should matching appropriately',
-      function() {
-         var version1Spy = sinon.spy();
-         var version2Spy = sinon.spy();
-         var middleware = routesVersioning({
-            "^1.2.1": version1Spy,
-            "2.3.1": version2Spy
-         });
-         req.version = "1.4.2";
-         middleware(req, res, next);
-         assert.ok(version1Spy.calledOnce);
-         assert.ok(version1Spy.calledWith(req, res, next));
-      });
-      it('when ~ is used in version, version should matching appropriately',
-         function() {
-            var version1Spy = sinon.spy();
-            var version2Spy = sinon.spy();
-            var middleware = routesVersioning({
-               "~1.2.1": version1Spy,
-               "2.3.1": version2Spy
-            });
-            req.version = "1.2.9";
-            middleware(req, res, next);
-            assert.ok(version1Spy.calledOnce);
-            assert.ok(version1Spy.calledWith(req, res, next));
-         });
-         it('when ~ is used in version, version should matching appropriately, '+
-         'if doesnt match, highest version should be called',
-            function() {
-               var version1Spy = sinon.spy();
-               var version2Spy = sinon.spy();
-               var middleware = routesVersioning({
-                  "~1.2.1": version1Spy,
-                  "2.3.1": version2Spy
-               });
-               req.version = "1.3.9";
-               middleware(req, res, next);
-               assert.ok(version2Spy.calledOnce);
-               assert.ok(version2Spy.calledWith(req, res, next));
-            });
-    it('when version provided as integer, version should cast to string',
-      function() {
-         var version1Spy = sinon.spy();
-         var middleware = routesVersioning({
-            "1": version1Spy,
-         });
-         req.version = 1;
-         middleware(req, res, next);
-         assert.ok(version1Spy.calledOnce);
-         assert.ok(version1Spy.calledWith(req, res, next));
-      });
+      app.request.version = '1.3.9';
+      middleware(app);
+      assert.ok(version2Spy.calledOnce);
+      assert.ok(version2Spy.calledWith(app));
+    }
+  );
+  it('when version provided as integer, version should cast to string', function() {
+    const version1Spy = sinon.spy();
+    const middleware = routesVersioning({
+      '1': version1Spy,
+    });
+    app.request.version = 1;
+    middleware(app);
+    assert.ok(version1Spy.calledOnce);
+    assert.ok(version1Spy.calledWith(app));
+  });
 });
